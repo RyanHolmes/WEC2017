04154ac7943962f9240f78d3caff67f2de79b21b
Hannes Filler <hfiller2@uwo.ca>
2017-01-13 20:00:05 -0500

    combined files, test succeeded

--- problem_2/reader.js
+++ problem_2/reader.js
@@ -1,5 +1,7 @@
 var fs = require('fs');
-var textInput = fs.readFileSync('test1.txt','utf8');
+var textInput = fs.readFileSync('input/test1.txt','utf8');
+
+var heuristic = require('./app.js');
 
 var OPEN_TILE_ASCII_VALUE = 30;
 var ASCII_OFFSET = 65;
@@ -9,10 +11,12 @@ var tempMatrix = textInput.split('\n');
 var initialMatrix = [];
 for (var i = 0; i < 4; i++){
   initialMatrix[i] = tempMatrix[i].split('').map(function(e){
-    if(e.charCodeAt(0) - OPEN_TILE_ASCII_VALUE === OPEN_TILE) {
+    if(e.charCodeAt(0) - ASCII_OFFSET === OPEN_TILE_ASCII_VALUE) {
       return OPEN_TILE;
     } else {
-      return e.charCodeAt(0) - OPEN_TILE_ASCII_VALUE;
+      return e.charCodeAt(0) - ASCII_OFFSET;
     }
   });
 }
+
+console.log(heuristic(initialMatrix));

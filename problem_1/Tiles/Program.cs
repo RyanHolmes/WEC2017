aac46b9394f30abdd4ade9a9442cd99387cfdc15
Ian Wood <ian.wood108@gmail.com>
2017-01-13 21:04:02 -0500

    Created project for problem 1

---
+++ problem_1/Tiles/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tiles {
+    class Program {
+        static void Main(string[] args) {
+            System.Console.WriteLine("Hello world!");
+            System.Console.ReadKey();
+        }
+    }
+}
